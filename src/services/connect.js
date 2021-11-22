3d05f4209bc9f7d91cacb905dbd0a1b08d0bcee4
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2021-11-22 09:44:06 -0300

    tst

--- src/services/connect.js
+++ src/services/connect.js
@@ -1,5 +1,5 @@
 const axios = require('axios').default;
 
-const connect = axios.create({ baseURL: `https://0681-2804-3a8-86e-c400-89d7-c29a-4bb1-90ba.ngrok.io` });
+const connect = axios.create({ baseURL: `https://ab67-2804-3a8-86e-c400-89d7-c29a-4bb1-90ba.ngrok.io` });
 
 module.exports = connect;
