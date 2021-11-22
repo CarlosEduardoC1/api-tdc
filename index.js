2ebc9b2fdf91ff62494c9b1dd50938a548939b42
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2021-11-22 09:11:14 -0300

    api local

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/chat', chatRouter);
 app.use('/files', filesRouter);
 
 
-app.listen(process.env.PORT || 3001, function () {
+app.listen(process.env.PORT || 4000, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
--- src/services/connect.js
+++ src/services/connect.js
@@ -1,5 +1,5 @@
 const axios = require('axios').default;
 
-const connect = axios.create({ baseURL: `http://localhost:3000` });
+const connect = axios.create({ baseURL: `https://4af6-2804-3a8-86e-c400-89d7-c29a-4bb1-90ba.ngrok.io` });
 
 module.exports = connect;
