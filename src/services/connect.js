15cb01ecefec7a402b45024435f7cbe12598619f
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2021-11-22 21:54:36 -0300

    link

--- src/services/connect.js
+++ src/services/connect.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 
 const connect = axios.create({ 
-    baseURL: `https://8217-2804-3a8-86e-c400-89d7-c29a-4bb1-90ba.ngrok.io/`
+    baseURL: `https://d90b-2804-3a8-849-c000-f0cf-10b4-aaff-70fe.ngrok.io/`
     // baseURL: `http://192.168.1.6:5000`
  });
 
